cd0f3dfff6cbcaaeaf6638239e8cda93d236d5d2
Matan Genezia <gmatan.320@gmail.com>
2018-04-22 15:07:08 +0300

    Initial Commit

---
+++ MGRadius/MGRadius.h
@@ -0,0 +1,19 @@
+//
+//  MGRadius.h
+//  MGRadius
+//
+//  Created by Matan Genezia on 22/04/2018.
+//  Copyright © 2018 Matan Genezia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGRadius.
+FOUNDATION_EXPORT double MGRadiusVersionNumber;
+
+//! Project version string for MGRadius.
+FOUNDATION_EXPORT const unsigned char MGRadiusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGRadius/PublicHeader.h>
+
+
